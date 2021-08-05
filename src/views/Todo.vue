47725f9e047a8f53083183878548260198248dfa
takashiono2 <takashiono710@gmail.com>
2021-08-05 19:25:28 +0900

    add task form

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="home" >
+    <v-text-field
+      v-model="newTaskTilte"
+      lass="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      clearable
+      hide-details
+    ></v-text-field>
     <v-list class="pt-0">
       <div
           v-for="task in tasks" 
           :key="task.id"   
       >
-        <v-list-item @click="doneTask(task.id)">
+        <v-list-item 
+          @click.stop="doneTask(task.id)"
+          :class= "{ 'blue lighten-4': task.done }"
+        >
           <template v-slot:default>
             <v-list-item-action>
               <v-checkbox
@@ -15,8 +27,19 @@
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{task.title}}</v-list-item-title>
+              <v-list-item-title 
+              :class="{'text-decoration-line-through': task.done}">
+                {{task.title}}
+              </v-list-item-title>
             </v-list-item-content>
+            <v-list-item-action>
+              <v-btn 
+                @click.stop="deleteTask(task.id)"
+                icon
+              >
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
           </template>
         </v-list-item>
         <v-divider></v-divider>
@@ -30,6 +53,9 @@
     name: 'Todo',
     data: function(){
       return {
+        newTaskTilte:
+          'hello'
+        ,
         tasks: [
               {
                 id: 1,
@@ -53,6 +79,9 @@
       doneTask: function(id){
         let task = this.tasks.filter(task=>task.id === id)[0];
         task.done = !task.done;
+      },
+      deleteTask: function(id){
+        this.tasks = this.tasks.filter(task=>task.id !== id);
       }
     }
   }
